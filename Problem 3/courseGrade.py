69d8d0412bbe362fcbb122038b449e71d28662cb
Kristy <KristyTR30@gmail.com>
2024-03-11 10:14:41 -0500

    Nhi Tran

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -8,58 +8,33 @@ def compute_grade(score):
     elif score >= 60:
         return 'D'
     else:
-        return 'F'
+        return 'F'      
 
-def courseGrade():
-    while True:
-        try:
-            file_name = input("Enter  tsv: ").strip()
-            if file_name.lower() == 'quit':
-                break
-            
-            midterm1_total = 0
-            midterm2_total = 0
-            final_total = 0
-            student_count = 0
+def compute_average(scores):
+    return sum(scores)/len(scores)
 
-            with open(file_name, 'r') as file:
-                lines = file.readlines()
+filename = input("Enter the tsv filename: ")
+students = []
+with open(filename, 'r') as f:
+    for line in f:
+        fields = line.strip().split('\t')
+        last_name, first_name, midterm1, midterm2, final = fields
+        midterm1 = int(midterm1)
+        midterm2 = int(midterm2)
+        final = int(final)
+        students.append((last_name, first_name, midterm1, midterm2, final))
 
-            output_file_name = f'report_{file_name.split(".")[0]}.txt'
+with open('report.txt', 'w') as f:
+    for student in students:
+        last_name, first_name, midterm1, midterm2, final = student
+        avg_score = compute_average([midterm1, midterm2, final])
+        letter_grade = compute_grade(avg_score)
+        f.write(f"{last_name}\t{first_name}\t{midterm1}\t{midterm2}\t{final}\t{letter_grade}\n")
 
-            with open(output_file_name, 'w') as report_file:
-                for line in lines:
-                    parts = line.split('\t')
-                    last_name, first_name, midterm1, midterm2, final = parts
-                    midterm1 = int(midterm1)
-                    midterm2 = int(midterm2)
-                    final = int(final)
-                    
-                    average_score = (midterm1 + midterm2 + final) / 3
-                    
-                    letter_grade = compute_grade(average_score)
-                    
-                    report_file.write(f'{last_name}\t{first_name}\t{midterm1}\t{midterm2}\t{final}\t{letter_grade}\n')
-                    
-                    midterm1_total += midterm1
-                    midterm2_total += midterm2
-                    final_total += final
-                    student_count += 1
+exams = {'midterm1': [], 'midterm2': [], 'final': []}
+for student in students:
+    exams['midterm1'].append(student[2])
+    exams['midterm2'].append(student[3])
+    exams['final'].append(student[4])
 
-           
-            midterm1_avg = midterm1_total / student_count
-            midterm2_avg = midterm2_total / student_count
-            final_avg = final_total / student_count
-
-            with open(output_file_name, 'a') as report_file:
-                report_file.write(f'\nAverages: midterm1 {midterm1_avg:.2f}, midterm2 {midterm2_avg:.2f}, final {final_avg:.2f}')
-
-            print(f'Report file "{output_file_name}" has been generated successfully.\n')
-        
-        except FileNotFoundError:
-            print(f'Error: File "{file_name}" not found.\n')
-        except Exception as e:
-            print(f'An error occurred: {str(e)}')
-
-if __name__ == "__main__":
-    courseGrade()
+f.write(f"\nAverages: midterm1 {avg1:.2f}, midterm2 {avg2:.2f}, final {avg3:.2f}")
