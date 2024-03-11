46365e2ec9da91c3601cb00f80a0be5d3f27ff55
Kristy <KristyTR30@gmail.com>
2024-03-11 09:49:47 -0500

    Nhi Tran

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -13,24 +13,20 @@ def compute_grade(score):
 def courseGrade():
     while True:
         try:
-            file_name = input("Enter the name of the tsv file (or 'quit' to exit): ").strip()
+            file_name = input("Enter  tsv: ").strip()
             if file_name.lower() == 'quit':
                 break
-
-            # Initialize variables to store total scores and count of students
+            
             midterm1_total = 0
             midterm2_total = 0
             final_total = 0
             student_count = 0
 
-            # Read lines from the input file
             with open(file_name, 'r') as file:
                 lines = file.readlines()
 
-            # Generate output file name
             output_file_name = f'report_{file_name.split(".")[0]}.txt'
 
-            # Open output file to write student reports
             with open(output_file_name, 'w') as report_file:
                 for line in lines:
                     parts = line.split('\t')
@@ -39,27 +35,22 @@ def courseGrade():
                     midterm2 = int(midterm2)
                     final = int(final)
                     
-                    # Calculate average score
                     average_score = (midterm1 + midterm2 + final) / 3
                     
-                    # Assign letter grade
                     letter_grade = compute_grade(average_score)
                     
-                    # Write student report to output file
                     report_file.write(f'{last_name}\t{first_name}\t{midterm1}\t{midterm2}\t{final}\t{letter_grade}\n')
                     
-                    # Update total scores and student count
                     midterm1_total += midterm1
                     midterm2_total += midterm2
                     final_total += final
                     student_count += 1
 
-            # Calculate average scores
+           
             midterm1_avg = midterm1_total / student_count
             midterm2_avg = midterm2_total / student_count
             final_avg = final_total / student_count
 
-            # Append average scores to output file
             with open(output_file_name, 'a') as report_file:
                 report_file.write(f'\nAverages: midterm1 {midterm1_avg:.2f}, midterm2 {midterm2_avg:.2f}, final {final_avg:.2f}')
 
