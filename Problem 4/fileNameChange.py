dedc9a924ae83259bf87d65d331d508b7a911b31
Kristy <KristyTR30@gmail.com>
2024-03-15 15:44:53 -0500

    NHI TRAN

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -1,13 +1,17 @@
-import fileNameChange as r
-
 def fileNameChange():
-    file_name = input()
-    with open(file_name, 'r') as file:
-        photo_names = file.readlines()
-    for photo_name in photo_names:
-        modified_name = photo_name.strip().replace("_photo.jpg", "_info.txt")
-        print(modified_name)
+    try:
+        file_name = input("Enter the file name: ")  
+        with open(file_name, 'r') as file:
+            photo_names = file.readlines()
+        
+        for photo_name in photo_names:
+            modified_name = photo_name.strip().replace("_photo.jpg", "_info.txt")
+            print(modified_name)
 
+    except FileNotFoundError:
+        print("File not found. Please make sure the file exists and try again.")
+    except Exception as e:
+        print("An error occurred:", str(e))
 
 if __name__ == '__main__':
     fileNameChange()
